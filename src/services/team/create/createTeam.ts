52c598b0df666644d645cdf9da0ffcdafbdc485c
Giuliano Maciel <giulianomaciellopes@gmail.com>
2025-08-20 13:08:05 -0300

    refactor: arrumando a logica do crate

--- src/services/team/create/createTeam.ts
+++ src/services/team/create/createTeam.ts
@@ -1,20 +1,20 @@
 import { prisma } from "@/database/prisma-config";
 import { TeamBodySchemaInput } from "@/schemazod/team/create";
-import { updatePlayer } from "@/services/player";
 import { AppError } from "@/utils/AppEroor";
-import { PlayerRole, Role } from "@prisma/client";
+import { findPlayerById } from "@/utils/prismaHelpersutils";
+
 
 interface CreateTeamRequest {
   data: TeamBodySchemaInput;
 }
 
 export async function createTeam({ data }: CreateTeamRequest) {
-  const verifiCaptain = await prisma.team.findFirst({
-    where: { captainId: data.captainId },
-  });
-  if (verifiCaptain) {
-    throw new AppError("Ops  esse player ja e capitao de um time ");
-  }
+ const {player} = await  findPlayerById(data.captainId)
+
+ if(player.teamId){
+  throw new AppError(" Esse jogador já pertence a um time")
+ }
+
 
   const TeamCreate = await prisma.team.create({ data });
 
