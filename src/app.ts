9ca225fa9231041e057f730919db0e2534729292
Giuliano Maciel <giulianomaciellopes@gmail.com>
2025-08-28 14:37:16 -0300

    feat: insatlando cors para consumo do font

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from "express";
 import "express-async-errors";
 import { routes } from "./routes";
 import { errorHandling } from "./middleware";
+import cors from "cors"
 
 
 export const app = express();
 
+app.use(cors)
 app.use(express.json());
 
 
