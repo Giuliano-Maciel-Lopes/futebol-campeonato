e54e25c74ac8bbcf477528e53729f69e19dc6891
Giuliano Maciel <giulianomaciellopes@gmail.com>
2025-09-28 22:49:11 -0300

    test: antes de adicioanr rowwlback a updatesttus

--- src/services/match/updatestatus/updateStatus.ts
+++ src/services/match/updatestatus/updateStatus.ts
@@ -1,6 +1,7 @@
 import { prisma } from "@/database/prisma-config";
 import { MatchBodyUpdateStatusInput } from "@/schemazod/match/updatStatus";
 import { updateGroupsScores } from "@/services/groupScore";
+import { AppError } from "@/utils/AppEroor";
 import { findMatchById } from "@/utils/prismaHelpersutils";
 
 type updateStatusProps = {
@@ -10,6 +11,15 @@ type updateStatusProps = {
 
 export async function updateStatus({ data, id }: updateStatusProps) {
  const {match}=   await findMatchById(id);
+  if (data.status === "FINISHED") {
+    if (match.status !== "ONGOING") {
+      throw new AppError(
+        match.status === "FINISHED"
+          ? "Essa partida já foi finalizada"
+          : "Para finalizar uma partida ela precisa entrar em andamento primeiro"
+      );
+    }
+  }
 
  if(data.status === "FINISHED"){
    await updateGroupsScores({match})
--- src/services/team/create/createTeam.ts
+++ src/services/team/create/createTeam.ts
@@ -2,6 +2,8 @@ import { prisma } from "@/database/prisma-config";
 import { TeamBodySchemaInput } from "@/schemazod/team/create";
 import { AppError } from "@/utils/AppEroor";
 import { findPlayerById } from "@/utils/prismaHelpersutils";
+import { PopulateGropScore } from "../utils/populateGroupScore";
+
 
 // so adm pode criar time ta no middlawqare
 interface CreateTeamRequest {
@@ -17,6 +19,10 @@ export async function createTeam({ data }: CreateTeamRequest) {
 
 
   const TeamCreate = await prisma.team.create({ data });
+
+   if(data.groupId){
+  await PopulateGropScore({groupId:data.groupId , team:TeamCreate})
+ }
 
   await prisma.player.update({
     where: { id: data.captainId },
